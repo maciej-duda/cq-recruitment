9a3bc859ce4d396017b539f69e85c57f80012910
Maciej Duda <micioru@gmail.com>
2023-10-29 11:17:59 +0100

    add removal tests

--- cypress/e2e/toDo.cy.js
+++ cypress/e2e/toDo.cy.js
@@ -17,6 +17,31 @@ describe('To-Do List', () => {
     // Add and verify if item is added correctly
     TodoPage.addItem(addItem);
     TodoPage.verifyItem(addItem);
+    // Check the checkbox before item name to complete item
     TodoPage.completeItem();
+    // Check for list item state change to check if item was marked as completed
+    TodoPage.checkItemCompletion();
+  });
+
+  it('Should remove an item from the list', () => {
+    // Add and verify if item is added correctly
+    TodoPage.addItem(addItem);
+    TodoPage.verifyItem(addItem);
+    // Completes the item from the list and then remove all completed items
+    TodoPage.completeItem();
+    TodoPage.removeCompletedItems();
+    // Verify that the to-do item is removed
+    TodoPage.checkRemoval(addItem);
+  });
+
+  it('Should remove a to-do item by hovering and clicking the "X" button', () => {
+    // Add and verify if item is added correctly
+    TodoPage.addItem(addItem);
+    TodoPage.verifyItem(addItem);
+    // Completes the item from the list and then remove completed item
+    TodoPage.completeItem();
+    TodoPage.removeSingleItem();
+    // Verify that the to-do item is removed
+    TodoPage.checkRemoval(addItem);
   });
 });
--- cypress/pageObjects/todoPage.js
+++ cypress/pageObjects/todoPage.js
@@ -11,9 +11,26 @@ class TodoPage {
       cy.get('label').should('have.text', itemText);
     }
 
-    completeItem(itemText) {
-        cy.get('.toggle').click();
-      }
-  }
+    completeItem() {
+      cy.get('.toggle').check();
+    }
+
+    checkItemCompletion() {
+      cy.get('li.completed').should('exist')
+    }
+
+    removeCompletedItems(itemText) {
+      cy.get('.clear-completed').click();
+    }
+
+    checkRemoval(itemText) {
+      cy.get('label').should('not.exist');
+    }
+
+    removeSingleItem(itemText) {
+        // no other idea how to handle this - I know it's not the elegant way
+      cy.get('.destroy').click({ force: true })
+    }
+}
   
   export default new TodoPage();
